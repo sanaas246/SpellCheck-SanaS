65d60b5febd2b8ee69b015655b1175adeca955e5
sanaas246 <112659421+sanaas246@users.noreply.github.com>
2022-09-21 14:33:39 -0600

    Update spellcheck.py

--- spellcheck.py
+++ spellcheck.py
@@ -4,18 +4,100 @@
 # 2: aliceWords: a list containing all of the words from "AliceInWonderland.txt"
 
 import re  # Needed for splitting text with a regular expression
-
+import math
+import time # needed to find the time the function used
 
 def main():
     # Load data files into lists
     dictionary = loadWordsFromFile("data-files/dictionary.txt")
     aliceWords = loadWordsFromFile("data-files/AliceInWonderLand.txt")
 
-    # Print first 50 values of each list to verify contents
-    print(dictionary[0:50])
-    print(aliceWords[0:50])
+    # Main Menu
+    loop = True 
+    while loop: 
+        selection = getMenuSelection()
+
+        if selection == "1" or selection == "spell check a word (linear search)":
+            wordlinear(dictionary)
+        elif selection == "2" or selection == "spell check a word (binary search)":
+            wordbinary(dictionary)
+        elif selection == "3" or selection == "spell check alice in wonderland (linear search)":
+            alicelinear()
+        elif selection == "4" or selection == "spell check alice in wonderland (binary search)":
+            alicebinary()
+        elif selection == "5" or selection == "exit":
+            exit()
+        else: 
+            print("Please choose an option. ")
 # end main()
 
+# MENU FUNCTIONS
+def getMenuSelection():
+    # Menu Options
+    print(f"\n********MAIN MENU********")
+    print("1. Spell Check a Word (Linear Search)")
+    print("2. Spell Check a Word (Binary Search)")
+    print("3. Spell Check Alice In Wonderland (Linear Search)")
+    print("4. Spell Check Alice In Wonderland (Binary Search)")
+    print("5. Exit")
+    return input("\nChoose an option please: ").lower()
+
+def wordlinear(dictionary):
+    word = input("Please enter a word: ").lower()
+    print("\nLinear Search Starting...")
+    startTime = time.time()
+    result = linearSearch(dictionary, word)
+    endTime = time.time()
+    if result == -1:
+        print(f"{word} is NOT IN the dictionary. ({endTime-startTime}) seconds")
+    else:
+        print(f"{word} is IN the dictionary at position {result}. ({endTime-startTime}) seconds")
+
+
+def wordbinary(dictionary):
+    word = input("Please enter a word: ").lower()
+    print("\nBinary Search Starting...")
+    startTime = time.time()
+    result = binarySearch(dictionary, word)
+    endTime = time.time()
+    timePassed = endTime - startTime
+    if result == -1:
+        print(f"{word} is NOT IN the dictionary. ({timePassed}) seconds")
+    else:
+        print(f"{word} is IN the dictionary at position {result}. ({timePassed}) seconds")
+
+def alicelinear():
+    pass
+
+def alicebinary():
+    pass
+
+def exit():
+    loop = False
+    print("\nBye!")
+
+
+# HELPER FUNCTIONS
+# Binary and Linear Search Functions
+def linearSearch(anArray, item):
+    for i in range(len(anArray)):
+        if anArray[i] == item:
+            return i
+    return -1
+
+def binarySearch(anArray,item):
+    li = 0
+    ui = len(anArray)-1
+ 
+    while li < ui or li == ui:
+        mi = math.floor((li+ui)/2)
+        if item == anArray[mi]:
+            return mi
+        elif item < anArray[mi]:
+            ui = mi - 1
+        elif item > anArray[mi]:
+            li = mi + 1
+    return -1
 
 def loadWordsFromFile(fileName):
     # Read file as a string
@@ -26,6 +108,10 @@ def loadWordsFromFile(fileName):
     # Split text by one or more whitespace characters
     return re.split('\s+', textData)
 # end loadWordsFromFile()
+
+
+
+
 
 
 # Call main() to begin program
